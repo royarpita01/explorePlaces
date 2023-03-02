39fa2e96aa0f5d5b923506500fbf7b6f8bd6e2de
Arpita Roy <arpita653@gmail.com>
2023-03-02 14:44:18 +0600

    place commit!!

---
+++ app/src/main/java/com/example/exploreplaces/ExploreSylhet.java
@@ -0,0 +1,14 @@
+package com.example.exploreplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreSylhet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_sylhet);
+    }
+}
